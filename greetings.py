ef05b454d48cd6a01f7e1e1cb535a508767e12e7
Muhammad Bello Ayuba <talk2muhammadbello@gmail.com>
2024-12-15 10:16:24 -0500

    initial commit

---
+++ greetings.py
@@ -0,0 +1,1 @@
+print("Hello Muhammad Faisal! Good Evening!")
---
+++ oneliners.php
@@ -0,0 +1,5 @@
+<?php
+Print "Dreams dont work unless you do.\n";
+Print "Every day is a second chance to prove your alarm clock wrong.\n";
+Print "When life gives you lemons, make grape juice and leave the world wondering how you did it.\n";
+?>
